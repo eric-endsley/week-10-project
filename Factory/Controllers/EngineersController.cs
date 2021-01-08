f6d3dca126d986fee102411c2e424994c119b975
Eric Endsley <eric.endsley4@gmail.com>
2021-01-08 14:41:11 -0800

    create AddMachine get/post routes in engineers controller

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -44,5 +44,29 @@ namespace Factory.Controllers
                 .FirstOrDefault(engineer => engineer.EngineerId == id);
             return View(thisEng);
         }
+
+        public ActionResult AddMachine(int id)
+        {
+            var thisEng = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+            ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
+            return View(thisEng);
+        }
+
+        [HttpPost]
+        public ActionResult AddMachine
+        (Engineer engineer, int MachineId)
+        {
+            if (MachineId != 0)
+            {
+                var returnedJoin = _db.EngineerMachine
+                    .Any(join => join.MachineId == MachineId && join.EngineerId == engineer.EngineerId);
+                if (!returnedJoin)
+                {
+                    _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+                }
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Details", new { id = engineer.EngineerId });
+        }    
     }
 }
