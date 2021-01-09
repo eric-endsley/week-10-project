009ca48ad721e25cb27d0ea6a49a4f10b707b220
Eric Endsley <eric.endsley4@gmail.com>
2021-01-08 16:01:23 -0800

    display machine & engineer lists on homepage

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly FactoryContext _db;
+
+        public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
+
         public ActionResult Index()
         {
+            ViewBag.Engineers = _db.Engineers;
+            ViewBag.Machines = _db.Machines;
             return View();
         }
     }
